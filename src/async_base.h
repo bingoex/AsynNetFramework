c90dd93bbc1fbf75a973b176177ae4a4e7c9081f
bingoex <haibinwumr@163.com>
2016-08-30 23:54:06 +0800

    add base file but nothing

---
+++ src/async_base.h
@@ -0,0 +1,30 @@
+#ifndef _CM_NET_H_
+#define _CM_NET_H_
+
+#include <netinet/if_ether.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <stdlib.h>
+#include <net/ethernet.h>
+#include <net/if.h>       /* for ifconf */  
+#include <netinet/in.h>       /* for sockaddr_in */  
+#include <sys/socket.h>  
+#include <sys/types.h>  
+#include <sys/ioctl.h>  
+#include <ifaddrs.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif 
+
+
+#ifdef __cplusplus
+}
+#endif 
+
+
+#endif
